b29930dfda04ad618b83c462af202915e454b4fb
dpadilla90 <d_padilla.y@hotmail.com>
2023-07-14 16:58:00 -0400

    added images to the assets and constraints

--- ISP-1002-Project/ISP-1002-Project/MenuItemCell.swift
+++ ISP-1002-Project/ISP-1002-Project/MenuItemCell.swift
@@ -7,6 +7,9 @@
 import UIKit
 
 class MenuItemCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UILabel!
     
 }
 
