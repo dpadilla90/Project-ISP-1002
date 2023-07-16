03bcc8f77657984846c59a59d89d8d59f397c2cf
dpadilla90 <d_padilla.y@hotmail.com>
2023-07-16 17:17:16 -0400

    Added imaged for launch and icon

--- ISP-1002-Project/ISP-1002-Project/ViewController.swift
+++ ISP-1002-Project/ISP-1002-Project/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         
+            
+        
         let menuItem1 = MenuItem(itemID: 1, title: "Burger", description: "Delicious beef burger", image: "burger")
         let menuItem2 = MenuItem(itemID: 2, title: "Pizza", description: "Freshly baked pizza with assorted toppings", image: "pizza")
         let menuItem3 = MenuItem(itemID: 2, title: "Fried Chicken", description: "Crispy, flavorful fried chicken.", image: "chicken")
@@ -38,13 +40,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.titleLabel.text = menuItem.title
         cell.descriptionLabel.text = menuItem.description
         cell.itemImageView.image = UIImage(named: menuItem.image)
-        
+    
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         
-        return 140
+        return 100
     }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        headerView.backgroundColor = UIColor.lightGray
+        
+        let titleLabel = UILabel(frame: CGRect(x: 10, y: 0, width: tableView.frame.width - 20, height: 40))
+        titleLabel.text = "All You Can Eat Menu"
+        titleLabel.textColor = UIColor.black
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        headerView.addSubview(titleLabel)
+        
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+
 }
 
     
